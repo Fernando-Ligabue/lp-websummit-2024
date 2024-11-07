07370fd8dc4e493b13858fe70297569ce14ef558
Fernando-Ligabue <nandoligabue@gmail.com>
2024-11-07 11:19:27 +0000

    remove unused packages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/webSummit",
+  base: "/",
   build: {
     chunkFileNames: "./assets/[name].[hash].js",
     assetFileNames: "./assets/[name].[hash].[ext]",
